b5a28e400c66a95d8ec7e453f829197f69afb35d
Erik <erik.ondra@yahoo.com>
2024-09-21 16:35:58 +0200

    fluid font sizes for headlines

--- src/components/ui/headline.tsx
+++ src/components/ui/headline.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 
 type HeadlineProps = {
-    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-    className?: string;
-    children: React.ReactNode;
-};
-
-const fontSizeMap = {
-	h1: "text-5xl md:text-6xl lg:text-7xl xl:text-8xl",
-	h2: "text-4xl md:text-5xl lg:text-6xl",
-	h3: "text-3xl md:text-4xl lg:text-5xl",
-	h4: "text-2xl md:text-3xl lg:text-4xl",
-	h5: "text-xl md:text-2xl lg:text-3xl",
-	h6: "text-lg md:text-xl lg:text-2xl",
+	as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+	className?: string;
+	children: React.ReactNode;
 };
 
 const Headline = (props: HeadlineProps) => {
 	const { as: Tag = "h2", className = "", children } = props;
-	const fontSizeClass = fontSizeMap[Tag];
 
-	return <Tag className={`${fontSizeClass} font-poppins font-black ${className}`}>{children}</Tag>;
+	return <Tag className={`font-poppins font-black ${className}`}>{children}</Tag>;
 };
 
 export default Headline;
