89a21019c30fe75f4a03cabea95d5975a8eced93
Erik <erik.ondra@yahoo.com>
2024-10-20 09:03:57 +0200

    optimized language switcher and font

--- src/components/ui/LanguageSwitcher.tsx
+++ src/components/ui/LanguageSwitcher.tsx
@@ -1,10 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { Button } from "@/components/ui/button";
+import { clsx } from "clsx";
 
 const LanguageSwitcher = () => {
+	const [currentLanguage, setCurrentLanguage] = useState<"en" | "de" | undefined>(undefined);
+
+	useEffect(() => {
+		const locale = document.cookie
+			.split("; ")
+			.find((row) => row.startsWith("NEXT_LOCALE="))
+			?.split("=")[1];
+
+		if (locale === "de" || locale === "en") {
+			setCurrentLanguage(locale as "en" | "de");
+		}
+	}, []);
 
 	const changeLanguage = (locale: "en" | "de") => {
 		document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=${365 * 24 * 60 * 60}`;
@@ -12,6 +25,9 @@ const LanguageSwitcher = () => {
 		const newUrl = window.location.pathname.replace(/\/(en|de)\b/, `/${locale}`);
 		window.location.href = newUrl || "/";
 	};
+
+	const englishClassName = clsx("hover:bg-mauve-200 cursor-pointer rounded-md px-4 py-2", {"text-primary": currentLanguage === "en"});
+	const germanClassName = clsx("hover:bg-mauve-200 cursor-pointer rounded-md px-4 py-2", {"text-primary": currentLanguage === "de"});
 
 	return (
 		<DropdownMenu.Root>
@@ -23,13 +39,13 @@ const LanguageSwitcher = () => {
 					className="animate-slideDownAndFade min-w-[160px] rounded-md bg-white p-2 shadow-md"
 					sideOffset={5}>
 					<DropdownMenu.Item
-						className="hover:bg-mauve-200 cursor-pointer rounded-md px-4 py-2"
+						className={englishClassName}
 						onClick={() => changeLanguage("en")}>
 						English
 					</DropdownMenu.Item>
 
 					<DropdownMenu.Item
-						className="hover:bg-mauve-200 cursor-pointer rounded-md px-4 py-2"
+						className={germanClassName}
 						onClick={() => changeLanguage("de")}>
 						Deutsch
 					</DropdownMenu.Item>
